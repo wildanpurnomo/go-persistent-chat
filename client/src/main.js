35a0f19b9d83b1b0fd983dd15da3c8493f3b4efc
Wildan F Purnomo <wildanfajarpurnomo@gmail.com>
2021-09-15 05:50:06 +0700

    setup vuex

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import { createProvider } from './vue-apollo'
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -10,5 +11,6 @@ new Vue({
   vuetify,
   router,
   apolloProvider: createProvider(),
+  store,
   render: h => h(App)
 }).$mount('#app')
